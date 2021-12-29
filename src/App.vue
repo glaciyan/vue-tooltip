744a1f87aeb9b1da3d2ea3c28b6f96d4366e7845
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-29 20:36:57 +0100

    working tooltip

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,115 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { arrow, computePosition, flip, offset, shift } from "@floating-ui/dom";
+import { useElementHover, useFocus } from "@vueuse/core";
+import { computed, ref, watch } from "vue";
+
+const button = ref();
+
+const tooltip = ref();
+const arrowEl = ref();
+
+const position = ref({ x: 0, y: 0, arrow: {} });
+
+const showTooltip = useElementHover(button);
+
+const updateFloating = async () => {
+  const { x, y, placement, middlewareData } = await computePosition(
+    button.value,
+    tooltip.value,
+    {
+      placement: "top",
+      middleware: [
+        offset(8),
+        flip(),
+        shift({ padding: 4 }),
+        arrow({ element: arrowEl.value, padding: 6 }),
+      ],
+    }
+  );
+
+  const arrowD = middlewareData.arrow;
+
+  const staticSide = {
+    top: "bottom",
+    right: "left",
+    bottom: "top",
+    left: "right",
+  }[placement.split("-")[0]]!;
+
+  position.value = {
+    x,
+    y,
+    arrow: {
+      left: arrowD?.x != null ? `${arrowD.x}px` : "",
+      top: arrowD?.y != null ? `${arrowD.y}px` : "",
+      right: "",
+      bottom: "",
+      [staticSide]: "-4px",
+    },
+  };
+};
+
+watch(
+  () => showTooltip.value,
+  (show) => {
+    if (show) updateFloating();
+  }
+);
+</script>
 
 <template>
-  <h1 class="animate-spin">Hello!!</h1>
+  <div class="flex flex-col">
+    <p>asdfasdfa<br />sdfasdfasdf</p>
+    <button
+      ref="button"
+      aria-describedby="tooltip"
+      class="
+        px-3
+        py-2
+        bg-sky-400
+        text-white
+        rounded
+        hover:bg-sky-500
+        active:bg-sky-600
+        transition-colors
+        shadow
+        m-2
+      "
+    >
+      Send Message
+    </button>
+  </div>
+  <div
+    ref="tooltip"
+    role="tooltip"
+    :class="['top-0 left-0 transition-opacity', { 'opacity-0': !showTooltip }]"
+    :style="{
+      transform: `translate(${Math.round(position.x)}px,${Math.round(position.y)}px)`,
+    }"
+  >
+    My tooltip
+    <div class="arrow" ref="arrowEl" :style="[{}, position.arrow]"></div>
+  </div>
+  <p v-for="num in 300">num</p>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+div[role="tooltip"] {
+  position: absolute;
+  background: #222;
+  color: white;
+  font-weight: bold;
+  padding: 5px;
+  border-radius: 4px;
+  font-size: 90%;
+  pointer-events: none;
+}
+
+.arrow {
+  position: absolute;
+  background: #222;
+  width: 8px;
+  height: 8px;
+  transform: rotate(45deg);
 }
 </style>
