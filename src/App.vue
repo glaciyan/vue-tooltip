4db3a2397490aea2372a2a22b75c357fbf7f7bb1
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-12-30 11:58:43 +0100

    change up text

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,24 @@
 
 <template>
   <div class="flex flex-col">
-    <p>asdfasdfa<br />sdfasdfasdf</p>
+    <p>Some stuff<br />here and there</p>
     <button
       v-tooltip:bottom="'My Button'"
       ref="button"
       aria-describedby="tooltip"
-      class="
-        px-3
-        py-2
-        bg-sky-400
-        text-white
-        rounded
-        hover:bg-sky-500
-        active:bg-sky-600
-        transition-colors
-        shadow
-        m-2
-      "
+      class=""
     >
       Send Message
     </button>
   </div>
   <button v-tooltip="'The Other Button'">My Other Button</button>
-  <!-- <p v-for="num in 300">num</p> -->
+  <button v-tooltip:left="'The Other Button'">My Other Button</button>
+  <button class="block" v-tooltip:left="'The Other Button'">My Other Button</button>
+  <p v-for="num in 300">Filler Content</p>
 </template>
+
+<style scoped>
+button {
+  @apply px-3 py-2 bg-sky-400 text-white rounded hover:bg-sky-500 active:bg-sky-600 transition-colors shadow m-2;
+}
+</style>
